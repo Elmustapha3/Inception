c9678cb93af9c6606a19bb6b5baf9448a5d7409e
El mustapha EJ-JAMAAY <mustejjamaay2001@gmail.com>
2024-12-19 15:32:32 +0100

    last version

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,13 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
-# sleep 10
+echo "Initializing MariaDB..."
 
-cat << EOF > /etc/mysql/init.sql
-CREATE DATABASE IF NOT EXISTS ${DB_NAME};
-CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
-GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-EOF
+mkdir -p /run/mysqld
+chown mysql:mysql /run/mysqld
 
+mysql_install_db --user=mysql --ldata=/var/lib/mysql
 
-exec /usr/bin/mysqld_safe
+mysqld --datadir='/var/lib/mysql' &
+
+until mysqladmin ping --silent; do
+  echo "Waiting for MariaDB to be ready..."
+  sleep 2
+done
+
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' WITH GRANT OPTION;"
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+echo "Database and user created successfully."
+
+wait
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -3,16 +3,15 @@
 cd /var/www/html
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
 chmod +x wp-cli.phar
 
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST} --allow-root
-./wp-cli.phar core install --url=$URL --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root
-./wp-cli.phar user create "$NORMAL_USER" "$NORMAL_EMAIL" --user_pass="$NORMAL_PASSWORD" --role=editor --allow-root
+./wp-cli.phar config create --dbname=${DB_NAME} --dbuser=eej-jama --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST} --allow-root
+./wp-cli.phar core install --url=${URL} --title=${TITLE} --admin_user=${ADMIN_USER} --admin_password=${ADMIN_PASSWORD} --admin_email=${ADMIN_EMAIL} --allow-root
+./wp-cli.phar user create ${NORMAL_USER} ${NORMAL_EMAIL} --user_pass=${NORMAL_PASSWORD} --role=editor --allow-root
 
-mkdir /var/run/php 
+mkdir /var/run/php
 
-echo "listen=wordpress:9000" >> /etc/php/7.4/fpm/pool.d/www.conf
+echo "listen=9000" >> /etc/php/7.4/fpm/pool.d/www.conf
 
 exec php-fpm7.4 -F
